7e257ee389f6353e6f69e55dbe5774c12706a335
elias fernandes <eliasfernandescoutinho@gmail.com>
2021-06-03 22:37:46 -0300

    feat: add new views and save function usestate on readme

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
+  const [pessoa, setPessoa] = useState({
+    nome: "Elias Fernandes",
+    altura: 1.92,
+    idade: 30,
+    estado: "Paraná",
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{pessoa.nome}</Text>
+      <Text>{pessoa.idade}</Text>
+      <Button
+        title="click"
+        onPress={() => {
+          setPessoa({ ...pessoa, nome: "Jhess", idade: 27 });
+        }}
+      />
     </View>
   );
 }
@@ -14,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
